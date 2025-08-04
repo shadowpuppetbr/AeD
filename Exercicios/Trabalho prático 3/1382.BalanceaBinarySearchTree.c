6bcbb43bc3eb4f014fe81062174d9019c6c6a040
Gustavo <gustavoleon2001@gmail.com>
2025-08-03 21:38:00 -0300

    Trabalho prático 3: final feito em casa, sem ajuda de IAs

--- Exercicios/Trabalho prático 3/1382.BalanceaBinarySearchTree.c
+++ Exercicios/Trabalho prático 3/1382.BalanceaBinarySearchTree.c
@@ -1,63 +1,145 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+// Gustavo dos Santos Leon - M2
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Definition for a binary tree node.
+struct TreeNode
+{
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+struct TreeNode *balanceBST(struct TreeNode *root);
+void TreeNodeToArray(struct TreeNode *node, int *array, int *index);
+void insertNode(struct TreeNode *node, int value);
+struct TreeNode *createTree(int *array, int max, int min);
 
-int *TreeNodeToArray( struct TreeNode* root ){
-    if(root == NULL)
-      return 0;
+int main()
+{
+  struct TreeNode node4 = {4, NULL, NULL};
+  struct TreeNode node3 = {3, NULL, &node4};
+  struct TreeNode node2 = {2, NULL, &node3};
+  struct TreeNode node1 = {1, NULL, &node2};
 
-    int * array = malloc(sizeof(int)* 10000);
-    int index=0;
-
-    while(root != NULL){
-        array[index++] = root->val;
-
-        if(root->left != NULL) { 
-            array[index++] = root->left->val; 
-        } else { 
-                array[index++] = -1;
-        }
-        if(root->right) {
-            array[index++] = root->right->val;
-            
-        } else {
-                array[index++] = -1;
-        }
-        root = root->left;
-    }
-    for( int i =0 ; i< sizeof(array)/sizeof(int) ; i++) { 
-       printf("%d, ", array[i] );
-    }
-    return array;
+  struct TreeNode *balanced = balanceBST(&node1);
+  free(balanced);
+  return 0;
 }
 
-struct TreeNode* balanceBST(struct TreeNode* root) {
+struct TreeNode *createNode(int value)
+{
+  struct TreeNode *node = malloc(sizeof(struct TreeNode));
+  node->val = value;
+  node->left = NULL;
+  node->right = NULL;
+  return node;
+}
 
-    struct TreeNode *agent = root;
-    int * array = TreeNodeToArray(agent);
-/*
-    while(agent->left != NULL){
-        agent = agent->left;
-        deepL++;
-    }
-    while(agent->right != NULL){
-        agent = agent->right;
-        deepR++;
-    }
-    int diference = deepL - deepR;
+void TreeNodeToArray(struct TreeNode *node, int *array, int *index)
+{
+  if (node == NULL)
+    return;
 
-    if(  diference > 1 ){
-        liftUp(agent->left, agent );
-    } else {
-        liftUp(agent->right, agent );
-    }
-*/
+  // goes to left sub-tree first
+  if (node->left != NULL)
+    TreeNodeToArray(node->left, array, index);
 
-    return root;
+  // print or add value to array
+  array[*index] = node->val;
+  (*index)++;
+
+  // goes to right after
+  if (node->right != NULL)
+    TreeNodeToArray(node->right, array, index);
+}
+
+void insertNode(struct TreeNode *node, int value)
+{
+  // root vazio
+  if (node->val == -1)
+  {
+    node->val = value;
+    return;
+  }
+  // insere a esqueda
+  if (value < node->val)
+  { // se o node da esqurda não existir
+    if (node->left == NULL)
+    {
+      node->left = createNode(value);
+      return;
+    }
+    // se node a esquerda exister, for maior que value e tiver um irmão vazio
+    if (value < node->left->val && node->right == NULL)
+    {
+      node->right = createNode(node->val);
+      node->val = node->left->val;
+      node->left->val = value;
+    }
+    else
+    {
+      insertNode(node->left, value);
+    }
+  }
+  // insere a direita
+  else
+  {
+    // se o node da direita não existir
+    if (node->right == NULL)
+    {
+      node->right = createNode(value);
+      return;
+    }
+    // se node a direita exister, for menor que value e tiver um irmão vazio
+    if (value > node->right->val && node->left == NULL)
+    {
+      node->left = createNode(node->val);
+      node->val = node->right->val;
+      node->right->val = value;
+    }
+    else
+    {
+      insertNode(node->right, value);
+    }
+  }
+}
+
+struct TreeNode *createTree(int *array, int max, int min)
+{
+  int mid = (max - min) / 2 + min;
+  int mask = 0;
+  if (array[mid] == -1)
+  {
+    return NULL;
+  }
+  if (mid == 0)
+  {
+    struct TreeNode *newTreeRoot = createNode(array[mid]);
+    return newTreeRoot;
+  }
+  else
+  {
+    struct TreeNode *newTreeRoot = createNode(array[mid]);
+    mask = array[mid];
+    array[mid] = -1;
+
+    newTreeRoot->left = createTree(array, mid, min);
+    newTreeRoot->right = createTree(array, max, mid);
+
+    array[mid] = mask;
+
+    return newTreeRoot;
+  }
+}
+
+struct TreeNode *balanceBST(struct TreeNode *root)
+{
+  int index = 0;
+  int *array = malloc(sizeof(int) * 10000);
+  TreeNodeToArray(root, array, &index);
+
+  struct TreeNode *newTree = createTree(array, index, 0);
+  free(array);
+  return newTree;
 }
---
+++ Exercicios/Trabalho prático 3/Readme.md
@@ -0,0 +1,20 @@
+# trabalho prático 3
+
+Nome: Gustavo dos Santos Leon
+Turma: M2
+
+---
+
+**Descrição**
+Dada a raiz de uma árvore de busca binária, retorne uma árvore de busca binária balanceada com os mesmos valores de nós. Se houver mais de uma resposta, retorne qualquer uma delas.
+
+---
+
+**Exemplos**
+Input: root = [1,null,2,null,3,null,4,null,null]
+Output: [2,1,3,null,null,null,4]
+Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+
+**Principais dificuldades**
+Demorei um pouco para achar a solução, porem graças a insistência não consultei nenhuma ajuda de IA, ou material que entregasse a resposta.
+O professor pediu para depois de solucionar por conta própria implementar uma solução do editorial, porem não consegui implementar a versão mais complexa do editorial devido a ter demorado.
